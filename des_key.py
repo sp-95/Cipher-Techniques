a965a65f1aa2290a8282488281fb46552cf51c20
Shashanka Prajapati <shashankap95@gmail.com>
2016-10-26 21:24:34 +0530

    Upto Left Shift

--- des_key.py
+++ des_key.py
@@ -40,15 +40,27 @@ class DESKey:
         return self.__key
 
     def getKey(self, round=16):
-        for _ in range(round):
-            self.__toMatrix(self.__key)[:,:-1]
+        keyMatrix = self.__toMatrix(self.__key)[:,:-1]
+        for i in range(1, round+1):
+            if i in [1, 2, 9, 16]:
+                keyMatrix = np.vstack(self.__leftShift(keyMatrix[:5], 1), self.__leftShift(keyMatrix[5:], 1))
+            else:
+                keyMatrix = np.vstack(self.__leftShift(keyMatrix[:5], 2), self.__leftShift(keyMatrix[5:], 2))
 
     def __toMatrix(self, string):
         return np.array([c for c in string]).reshape(8, 8)
+    
+    def __leftShift(self, matrix, shift):
+        indices = range(shift, matrix.size) + range(shift)
+        return matrix.flatten().take(indices).reshape(matrix.shape)
+
 
 
 if __name__ == '__main__':  # if this file is being executed and not imported
     myObj = DESKey()
 
-    key = raw_input('Enter a 64-bit key: ')
-    myObj.setInitialKey(key)
+    # key = raw_input('Enter a 64-bit key: ')
+    # myObj.setInitialKey(key)
+
+    for i in range(1,17):
+        print('Round {}: {}'.format(i, myObj.getKey(i)))
