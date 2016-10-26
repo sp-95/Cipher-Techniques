6445caa05c43b3358beb968540780272d24aa104
Shashanka Prajapati <shashankap95@gmail.com>
2016-10-26 09:26:15 +0530

    Docstrings for Playfair Cipher

--- Playfair.py
+++ Playfair.py
@@ -1,18 +1,38 @@
+"""'
+Playfair Cipher
+=============
+
+Input:
+    Input string and a key string
+
+Output:
+    1. Key matrix based on the key string
+    2. Encrypted string
+    3. Decrypted string
+"""
+
+import __future__
+
+
+__author__ = 'Shashanka Prajapati'
+
+
 import re               # Regular Expression
 import numpy as np      # Handles n-dimentional lists
 
+
 class Playfair:
-    # Initialize class with no key
     def __init__(self):
+        """Initialize class with no key"""
         self.__generateMatrix('')
     
-    # Generate a 5x5 matrix based on the given key
     def __generateMatrix(self, key):
+        """Generate a 5x5 matrix based on the given key"""
         matrix = [j for i,j in enumerate(key) if j not in key[:i]]
         self.__matrix = np.array(matrix + [c for c in 'ABCDEFGHIKLMNOPQRSTUVWXYZ' if c not in matrix]).reshape(5,5)     # 'J' isn't included
     
-    # Split the input into pairs
     def __generatePairs(self, input):
+        """Split the input into pairs"""
         input = self.__validCharacters(input)
         pairs = []
 
@@ -29,8 +49,8 @@ class Playfair:
 
         return pairs
     
-    # Encrypt a pair using the key matrix
     def __encryptPair(self, input):
+        """Encrypt a pair using the key matrix"""
         first = self.__getCoordinates(input[0])
         second = self.__getCoordinates(input[1])
         
@@ -43,8 +63,8 @@ class Playfair:
         
         return cipher
     
-    # Decrypt a pair using the key matrix
     def __decryptPair(self, input):
+        """Decrypt a pair using the key matrix"""
         first = self.__getCoordinates(input[0])
         second = self.__getCoordinates(input[1])
         
@@ -57,33 +77,34 @@ class Playfair:
         
         return cipher
     
-    # Encryption
     def encrypt(self, input):
+        """Encryption"""
         pairs = self.__generatePairs(input)
         return ''.join([self.__encryptPair(pair) for pair in pairs])
     
-    # Decryption
     def decrypt(self, input):
+        """Decryption"""
         pairs = self.__generatePairs(input)
         return ''.join([self.__decryptPair(pair) for pair in pairs])
     
-    # Set a key value
     def setKey(self, key):
+        """Set a key value"""
         key = self.__validCharacters(key)
         self.__generateMatrix(key)
 
-    # Get the key matrix
     def getKey(self):
+        """Get the key matrix"""
         return self.__matrix
 
-    # Strip non-alphabetic characters and the letter 'J' from the input and convert it to uppercase
     def __validCharacters(self, input):
+        """Strip non-alphabetic characters and the letter 'J' from the input and convert it to uppercase"""
         return re.sub('[^A-Za-z]', '', input).upper().replace('J', 'I')
 
-    # Get the coordinates of a letter in the key matrix
     def __getCoordinates(self, letter):
+        """Get the coordinates of a letter in the key matrix"""
         x, y = np.where(self.__matrix==letter)
         return x[0], y[0]
+
 
 if __name__ == '__main__':  # if this file is being executed and not imported
     myObj = Playfair()
