e30dc79b0115fa1e14fde70c45925ab6284fe1fa
Shashanka Prajapati <shashankap95@gmail.com>
2016-10-27 10:56:21 +0530

    Working encryption

--- des1.py
+++ des1.py
@@ -30,6 +30,16 @@ class DESKey:
         """Initialize key"""
         self.__plainText = '0000001001000110100010101100111011101100101010000110010000100000'
         self.__key = '011110000011001111000011001000001101101001110000'
+        self.__e = [
+            31, 0, 1, 2, 3, 4,
+            3, 4, 5, 6, 7, 8,
+            7, 8, 9, 10, 11, 12,
+            11, 12, 13, 14, 15, 16,
+            15, 16, 17, 18, 19, 20,
+            19, 20, 21, 22, 23, 24,
+            23, 24, 25, 26, 27, 28,
+            27, 28, 29, 30, 31, 0,
+            ]
 
     def setKey(self, key):
         """Set the 48-bit key"""
@@ -47,13 +57,23 @@ class DESKey:
         """Get the 64-bit plain text"""
         return self.__plainText
 
+    def __expand(self, input):
+        return ''.join(np.array([c for c in input]).take(self.__e))
+
+    def __exor(self, input):
+        return str(bin(int(input, 2)^int(self.__key, 2)))[2:]
+
     def encrypt(self, input=None):
-        if re.search('[^01]', input):
-            raise DESKeyError('PlainText should contain only bits(0\'s and 1\'s)')
-        if len(input) != 64:
-            raise DESKeyError('Only 64-bit PlainTexts are accepted')
-        if input not None:
+        if input is not None:
+            if re.search('[^01]', input):
+                raise DESKeyError('PlainText should contain only bits(0\'s and 1\'s)')
+            if len(input) != 64:
+                raise DESKeyError('Only 64-bit PlainTexts are accepted')
             self.__plainText = input
+            return self.__exor(self.__expand(input[32:]))
+        else:
+            return self.__exor(self.__expand(self.__plainText[32:]))
+
 
 
 if __name__ == '__main__':  # if this file is being executed and not imported
@@ -67,5 +87,5 @@ if __name__ == '__main__':  # if this file is being executed and not imported
     # plain = raw_input('Enter the 64-bit PlainText: ')
     # myObj.encrypt(plain)
 
-    print('Key: {}'.format(myObj.getPlainText()))
-    myObj.encrypt()
+    print('Plain Text: {}'.format(myObj.getPlainText()))
+    print('Input for S-box: {}'.format(myObj.encrypt()))
